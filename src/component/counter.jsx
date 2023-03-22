b177ab31546f36d2877a717c899c5944358c11b9
Tine0814 <djhaybernardo@gmail.com>
2023-03-22 20:18:07 +0800

    try Update

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
 
   render() {
     return (
-      <div className="">
+      <div className="ako si dastine jhay">
         <div className="d-flex justify-content-center align-item-center mt-5 align-items-center">
           <span style={{ fontSize: 25 }} className={this.getBageClassName()}>
             {this.formatCount()}
